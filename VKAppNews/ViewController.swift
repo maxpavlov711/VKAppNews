d75baa533d90d264a0cea316d8c5ba77297b65e4
Max Pavlov <max.pavlov711@gmail.com>
2022-03-24 18:52:33 +0300

    Installing VKSDK

---
+++ VKAppNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKAppNews
+//
+//  Created by Max Pavlov on 24.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
