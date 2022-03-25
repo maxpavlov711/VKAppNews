5768123857280bda9f36855e6f3a4d30a86f8699
Max Pavlov <max.pavlov711@gmail.com>
2022-03-25 13:54:27 +0300

    API Networking logic

---
+++ VKAppNews/Services/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  VKAppNews
+//
+//  Created by Max Pavlov on 25.03.22.
+//
+
+import Foundation
+
+struct API {
+    static let scheme = "https"
+    static let host = "api.vk.com"
+    static let version = "5.131"
+    
+    static let newsFeed = "/method/newsfeed.get"
+}
