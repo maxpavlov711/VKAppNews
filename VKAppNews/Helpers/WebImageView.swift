35f9346c1ef71b791d7c3368d623a4bdcde697e0
Max Pavlov <max.pavlov711@gmail.com>
2022-03-29 16:22:14 +0300

    Loading and Caching Image

---
+++ VKAppNews/Helpers/WebImageView.swift
@@ -0,0 +1,40 @@
+//
+//  WebImageView.swift
+//  VKAppNews
+//
+//  Created by Max Pavlov on 29.03.22.
+//
+
+import UIKit
+
+class WebImageView: UIImageView {
+    
+    func set(imageUrl: String) {
+        guard let url = URL(string: imageUrl) else { return }
+        
+        if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
+            self.image = UIImage(data: cachedResponse.data)
+            print("from cache")
+            return
+        }
+        
+        print("from internet")
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            
+            DispatchQueue.main.async {
+                if let data = data, let response = response {
+                    self?.image = UIImage(data: data)
+                    self?.handleLoadedImage(data: data, response: response)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func handleLoadedImage(data: Data, response: URLResponse) {
+        guard let responseURL = response.url else { return }
+        let cachedResponse  = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: responseURL))
+    }
+}
